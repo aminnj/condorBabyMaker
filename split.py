0e4b303ba3c3b8d374087ef3b8da96f71206bc36
Nick Amin <namin@umail.ucsb.edu>
2014-07-10 15:08:40 -0700

    extra printouts

--- split.py
+++ split.py
@@ -7,14 +7,6 @@ if(len(sys.argv) < 6):
     print "python script dying :("
     sys.exit()
 
-print sys.argv
-# XXX pass in isMC wildcard babyName, numChunks,proxyFile
-#wildcard = "/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23/*1.root"
-#numChunks = 3
-#isMC=(sys.argv[1].lower() == "mc")
-#babyName="baby_TEST"
-#proxyFile="/tmp/x509up_u31567"
-
 isMC=(sys.argv[1].lower() == "mc")
 wildcard = sys.argv[2]
 babyName = sys.argv[3]
@@ -27,7 +19,9 @@ files = glob.glob(wildcard)
 print ">>> Is this MC?: %s" % isMC
 print ">>> Baby name (and folder name): %s" % babyName
 print ">>> Input files: %s" % resources
+print ">>> Number of files: %i" % len(files)
 print ">>> Number of chunks: %i" % numChunks
+print ">>> Proxy file: %s" % proxyFile
 
 condorFileStr = """
 universe = vanilla
@@ -64,12 +58,11 @@ doAllStrEnd = """
 doAllStrEnd = doAllStrEnd.replace("IS_MC", str(isMC).lower())
 condorFileStr = condorFileStr.replace("PROXY_FILE", proxyFile)
 
-#for chunk in range(3,4): # FIXME range(numChunks):
 for chunk in range(numChunks):
     folderName = babyName + "_" + str(chunk).zfill(3)
     condorName = babyName + "_" + str(chunk).zfill(3) + ".sh"
 
-    print ">>> Making", folderName
+    print ">>> Making", folderName, "for chunk",chunk,"with",len(chunks(files,numChunks)[chunk]),"files"
     os.system("mkdir -p %s" % folderName)
     os.system("cp -p %s/ScanChain.* %s/" % (resources, folderName))
     os.system("cp -rp %s/CORE %s/" % (resources, folderName))
@@ -78,15 +71,11 @@ for chunk in range(numChunks):
     fdoAll = open("%s/doAll.C" % folderName, "w")
     fcondorFile = open("%s/condorFile" % folderName, "w")
 
-    #print doAllStrBegin
     fdoAll.write( doAllStrBegin )
     for fileName in chunks(files, numChunks)[chunk]:
-        #print "    ch->Add(\"%s\");" % fileName
         fdoAll.write( "    ch->Add(\"%s\");\n" % fileName )
-    #print doAllStrEnd.replace("BABY_NAME", folderName)
     fdoAll.write( doAllStrEnd.replace("BABY_NAME", folderName) )
 
-    #print condorFileStr.replace("CONDOR_EXECUTABLE", condorName)
     fcondorFile.write( condorFileStr.replace("CONDOR_EXECUTABLE", condorName) )
     print ">>> Made", folderName
 
